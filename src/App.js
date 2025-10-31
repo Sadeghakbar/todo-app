d7a027c9fb6905eff2e80213a82a22df15c7421a
Sadegh Akbar <ar.sadeghakbar@gmail.com>
2025-10-31 11:37:30 +0100

    added todo-list component UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Input from './components/input/input';
+import TodoList from './components/todo-list/todo-list';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     
     <div className="App">
       <Input/>
+      <TodoList/>
       
     </div>
   );
---
+++ src/components/todo-list/todo-list.jsx
@@ -0,0 +1,12 @@
+import './todo-list.css';
+
+function TodoList() {
+  return (
+    
+    <div className='todo-list-container'>
+     
+    </div>
+  );
+}
+
+export default TodoList;
