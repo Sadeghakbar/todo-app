070bd5711adad30bde5045c0e3ec7edc46537c0d
Sadegh Akbar <ar.sadeghakbar@gmail.com>
2025-10-31 11:17:50 +0100

    added input and button UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from './components/input/input';
+
 
 function App() {
   return (
     
     <div className="App">
-      
+      <Input/>
       
     </div>
   );
---
+++ src/components/input/input.jsx
@@ -0,0 +1,16 @@
+
+import './input.css';
+
+function Input() {
+  return (
+    
+    <div className='input-container'>
+      <input className='input' placeholder='Add Your Task' />
+      <button className='submit'>Add</button>
+
+      
+    </div>
+  );
+}
+
+export default Input;
